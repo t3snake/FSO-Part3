c146b592081830d6d5421515de4db74846d73a7b
t3snake <t3snake@gmail.com>
2024-04-16 18:56:07 +0400

    Finish ex 3.9

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 morgan.token('body', function getResponse (req, res) {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
