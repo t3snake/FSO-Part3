322914da29b089eaaf1557da299322f2b8ba9c26
t3snake <t3snake@gmail.com>
2024-04-16 19:34:42 +0400

    Finish ex 3.11

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', function getResponse (req, res) {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => {
